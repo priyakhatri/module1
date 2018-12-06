637827990770bb057c414b61cdcd5fa09f60a9a9
priyakhatri <priya.khatri90@gmail.com>
2018-12-06 18:07:47 +0530

    package 1

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const controller1 = require('./controllers/controller1');
+const controller2 = require('./controllers/controller2');
+
+const shared = require('priya-shared-package');
+
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+
+app.get('/1', (req, res) => {
+  controller1(req, res);
+});
+
+app.get('/2', (req, res) => {
+  controller2(req, res);
+});
+
+app.use('/shared', shared);
+
+app.locals.name  = require(__dirname + '/package.json').name;
+
+module.exports = app;
